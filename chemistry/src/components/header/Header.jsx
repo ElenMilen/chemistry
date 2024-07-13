0a569ffa73d404c7257bc77b0055d6a9bdc6863b
ElenMilen <sikolo2007@gmail.com>
2024-07-12 21:46:11 -0700

    tabs

--- chemistry/src/components/header/Header.jsx
+++ chemistry/src/components/header/Header.jsx
@@ -1,14 +1,16 @@
 import { Link } from 'react-router-dom';
 import './header.scss';
-
+import { useLocation } from 'react-router-dom';
 const Header = () => {
+    const location = useLocation();
+    const currentPath = location.pathname;
   return (
     <div className='header'>
       <Link to='/'>
-        <span className='header-title'> Periodic Table </span>
+        <span className={`header-title ${currentPath === "/" || currentPath === "/chemistry" ? "active" : ""}`}> Periodic Table </span>
       </Link>
       <Link to='/reaction'>
-        <span className='header-title'> Reaction </span>
+        <span className={`header-title ${currentPath === "/reaction" ? "active" : ""}`}> Reaction </span>
       </Link>
     </div>
   )
