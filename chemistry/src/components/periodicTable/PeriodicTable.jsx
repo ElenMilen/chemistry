908fecbd2f23d00c358cc1fc58ce86b6fe258699
ElenMilen <sikolo2007@gmail.com>
2024-07-26 20:10:30 -0700

    updates

--- chemistry/src/components/periodicTable/PeriodicTable.jsx
+++ chemistry/src/components/periodicTable/PeriodicTable.jsx
@@ -2,12 +2,14 @@ import React, { useState } from "react";
 import "./periodicTable.scss"; // Import CSS file for styling
 import Modal from "../modal/Modal"; // Import the Modal component
 import PeriodicTableJSON from "../../PeriodicTableJSON.json";
+import periodic from "../../images/periodicTable.gif";
 
 const PeriodicTable = () => {
   const [selectedElement, setSelectedElement] = useState(
     PeriodicTableJSON.elements[0]
   );
   const [showModal, setShowModal] = useState(false);
+  const [showIntroModal, setShowIntroModal] = useState(true);
 
   const handleElementClick = (element) => {
     setSelectedElement(element);
@@ -16,28 +18,49 @@ const PeriodicTable = () => {
 
   const handleCloseModal = () => {
     setShowModal(false);
+    setShowIntroModal(false);
   };
 
   return (
-    <div className="periodic-table">
-      {PeriodicTableJSON.elements.map((element) => (
-        <div
-          style={{ backgroundColor: element.color }}
-          key={element.id}
-          className={`element element-${element.number}`}
-          onClick={() => handleElementClick(element)}
-        >
-          <div className="element-symbol">{element.symbol}</div>
-          <div className="element-name">{element.name}</div>
-        </div>
-      ))}
-      <Modal
-        show={showModal}
-        handleClose={handleCloseModal}
-        element={selectedElement}
-        title={selectedElement.name}
-      />
-    </div>
+      <>
+          <div className="filterBox">
+                <div className="filterCenter">
+                    <div className="alkalineMetals"></div>
+                </div>
+          </div>
+          <div className="periodic-table">
+              {PeriodicTableJSON.elements.map((element) => (
+                  <div
+                      style={{backgroundColor: element.color}}
+                      key={element.id}
+                      className={`element element-${element.number}`}
+                      onClick={() => handleElementClick(element)}
+                  >
+                      <div className="element-symbol">{element.symbol}</div>
+                      <div className="element-name">{element.name}</div>
+                  </div>
+              ))}
+              <Modal
+                  show={showModal}
+                  handleClose={handleCloseModal}
+                  element={selectedElement}
+                  title={selectedElement.name}
+              />
+              <Modal
+                  show={showIntroModal}
+                  handleClose={handleCloseModal}
+                  element={selectedElement}
+                  title={'How To Use:'}
+              >
+                  <div className="theGif">
+                      <span> Press on an element to see it's interactable model and other key details. </span>
+                      <img src={periodic} alt="gif" width="100%"
+                      />
+                  </div>
+              </Modal>
+          </div>
+      </>
+
   );
 };
 
